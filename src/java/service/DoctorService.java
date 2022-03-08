313397b964ac54fc61e017bd15daa55c04500988
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-03-08 15:32:59 -0700

    Create DoctorService.java

---
+++ src/java/service/DoctorService.java
@@ -0,0 +1,43 @@
+package service;
+
+import dataaccess.DoctorDB;
+import java.util.List;
+import models.Doctor;
+
+public class DoctorService {
+    public List<Doctor> getAll() throws Exception {
+        DoctorDB doctorDB = new DoctorDB();
+        List<Doctor> doctors = doctorDB.getAll();
+        return doctors;
+    }
+    
+    public Doctor get(int doctor_id) throws Exception {
+        DoctorDB doctorDB = new DoctorDB();
+        Doctor doctor = doctorDB.get(doctor_id);
+        return doctor;
+    }
+    
+    public void insert(int doctor_id, String first_name, String last_name, String email, String mobile_phone,
+            String alt_phone, String pref_contact_type, int account_id, String gender, String birth_date,
+            String street_address, String city, String province, String postal_code) throws Exception {
+        DoctorDB doctorDB = new DoctorDB();
+        Doctor doctor = new Doctor(doctor_id, first_name, last_name, email, mobile_phone, alt_phone,
+                pref_contact_type, account_id, gender, birth_date, street_address, city, province, postal_code);
+        doctorDB.insert(doctor);
+    }
+    
+    public void update(int doctor_id, String first_name, String last_name, String email, String mobile_phone,
+            String alt_phone, String pref_contact_type, int account_id, String gender, String birth_date,
+            String street_address, String city, String province, String postal_code) throws Exception {
+        DoctorDB doctorDB = new DoctorDB();
+        Doctor doctor = new Doctor(doctor_id, first_name, last_name, email, mobile_phone, alt_phone,
+                pref_contact_type, account_id, gender, birth_date, street_address, city, province, postal_code);
+        doctorDB.update(doctor);
+    }
+    
+    public void delete(int doctor_id) throws Exception {
+        Doctor doctor = get(doctor_id);
+        DoctorDB doctorDB = new DoctorDB();
+        doctorDB.delete(doctor);
+    }
+}
