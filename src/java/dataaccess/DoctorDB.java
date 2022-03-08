ac9c3e9ad1ef36549df5f5b8f01e1e6ee0e6246e
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-03-08 15:25:19 -0700

    Create DoctorDB.java

---
+++ src/java/dataaccess/DoctorDB.java
@@ -0,0 +1,167 @@
+package dataaccess;
+
+import java.sql.*;
+import java.util.*;
+import models.Doctor;
+
+public class DoctorDB {
+    public List<Doctor> getAll() throws Exception {
+        List<Doctor> doctors = new ArrayList<>();
+        ConnectionPool cp = ConnectionPool.getInstance();
+        Connection con = cp.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;        
+        String sql = "SELECT * FROM doctor";
+        
+        try {
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            
+            while(rs.next()) {
+                int doctor_id = rs.getInt(1);
+                String first_name = rs.getString(2);
+                String last_name = rs.getString(3);
+                String email = rs.getString(4); 
+                String mobile_phone = rs.getString(5);
+                String alt_phone = rs.getString(6);    
+                String pref_contact_type = rs.getString(7); 
+                int account_id = rs.getInt(8);
+                String gender = rs.getString(9);  
+                String birth_date = rs.getString(10);
+                String street_address = rs.getString(11);
+                String city = rs.getString(12); 
+                String province = rs.getString(13);
+                String postal_code = rs.getString(14);            
+                
+                Doctor doctor = new Doctor(doctor_id, first_name, last_name, email, mobile_phone, alt_phone,
+                        pref_contact_type, account_id, gender, birth_date, street_address, city, province, postal_code);
+                doctors.add(doctor);
+            }
+        } finally {
+            DBUtil.closeResultSet(rs);
+            DBUtil.closePreparedStatement(ps);
+            cp.freeConnection(con);
+        }
+        
+        return doctors;
+    }
+    
+    public Doctor get(int doctor_id) throws Exception {
+        Doctor doctor = null;
+        ConnectionPool cp = ConnectionPool.getInstance();
+        Connection con = cp.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        String sql = "SELECT * FROM doctor WHERE doctor_id = ?";
+        
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, doctor_id);
+            rs = ps.executeQuery();
+            
+            if(rs.next()) {
+                String first_name = rs.getString(2);
+                String last_name = rs.getString(3);
+                String email = rs.getString(4); 
+                String mobile_phone = rs.getString(5);
+                String alt_phone = rs.getString(6);    
+                String pref_contact_type = rs.getString(7); 
+                int account_id = rs.getInt(8);
+                String gender = rs.getString(9);  
+                String birth_date = rs.getString(10);
+                String street_address = rs.getString(11);
+                String city = rs.getString(12); 
+                String province = rs.getString(13);
+                String postal_code = rs.getString(14);   
+                
+                doctor = new Doctor(doctor_id, first_name, last_name, email, mobile_phone, alt_phone,
+                        pref_contact_type, account_id, gender, birth_date, street_address, city, province, postal_code);
+            }
+        } finally {
+            DBUtil.closeResultSet(rs);
+            DBUtil.closePreparedStatement(ps);
+            cp.freeConnection(con);
+        }
+        
+        return doctor;
+    }
+    
+    public void insert(Doctor doctor) throws Exception {
+        ConnectionPool cp = ConnectionPool.getInstance();
+        Connection con = cp.getConnection();
+        PreparedStatement ps = null;
+        String sql = "INSERT INTO doctor "
+                + "(doctor_id, first_name, last_name, email, mobile_phone, alt_phone, pref_contact_type, "
+                + "account_id, gender, birth_date, street_address, city, province, postal_code)"
+                + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, doctor.getDoctor_id());
+            ps.setString(2, doctor.getFirst_name());
+            ps.setString(3, doctor.getLast_name());
+            ps.setString(4, doctor.getEmail());  
+            ps.setString(5, doctor.getMobile_phone());
+            ps.setString(6, doctor.getAlt_phone());
+            ps.setString(7, doctor.getPref_contact_type());
+            ps.setInt(8, doctor.getAccount_id());
+            ps.setString(9, doctor.getGender());
+            ps.setString(10, doctor.getBirth_date());
+            ps.setString(11, doctor.getStreet_address());
+            ps.setString(12, doctor.getCity());
+            ps.setString(13, doctor.getProvince());
+            ps.setString(14, doctor.getPostal_code());
+            ps.executeUpdate();
+        } finally {
+            DBUtil.closePreparedStatement(ps);
+            cp.freeConnection(con);
+        }
+    }
+    
+    public void update(Doctor doctor) throws Exception {
+        ConnectionPool cp = ConnectionPool.getInstance();
+        Connection con = cp.getConnection();
+        PreparedStatement ps = null;
+        String sql = "UPDATE doctor SET doctor_id = ?, first_name = ?, last_name = ?, email = ?, " +
+                "mobile_phone = ?, alt_phone = ?, pref_contact_type = ?, account_id = ?, gender = ?, " +
+                "birth_date = ?, street_address = ?, city = ?, province = ?, postal_code = ?";
+        
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, doctor.getDoctor_id());
+            ps.setString(2, doctor.getFirst_name());
+            ps.setString(3, doctor.getLast_name());
+            ps.setString(4, doctor.getEmail());  
+            ps.setString(5, doctor.getMobile_phone());
+            ps.setString(6, doctor.getAlt_phone());
+            ps.setString(7, doctor.getPref_contact_type());
+            ps.setInt(8, doctor.getAccount_id());
+            ps.setString(9, doctor.getGender());
+            ps.setString(10, doctor.getBirth_date());
+            ps.setString(11, doctor.getStreet_address());
+            ps.setString(12, doctor.getCity());
+            ps.setString(13, doctor.getProvince());
+            ps.setString(14, doctor.getPostal_code());
+            ps.executeUpdate();
+        } finally {
+            DBUtil.closePreparedStatement(ps);
+            cp.freeConnection(con);
+        }
+    }
+    
+    public void delete(Doctor doctor) throws Exception {
+        ConnectionPool cp = ConnectionPool.getInstance();
+        Connection con = cp.getConnection();
+        PreparedStatement ps = null;
+        String sql = "DELETE FROM doctor WHERE doctor_id = ?";
+        
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, doctor.getDoctor_id());
+            ps.executeUpdate();
+        } finally {
+            DBUtil.closePreparedStatement(ps);
+            cp.freeConnection(con);
+        }
+    }
+}
