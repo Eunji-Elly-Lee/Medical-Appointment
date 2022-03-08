a3ae9e5a936ff9f163b1188668f60b59931d0476
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-03-07 19:27:01 -0700

    Create ConnectionPool.java

---
+++ src/java/dataaccess/ConnectionPool.java
@@ -0,0 +1,44 @@
+package dataaccess;
+
+import javax.sql.DataSource;
+import javax.naming.*;
+import java.sql.*;
+
+public class ConnectionPool {
+    private static ConnectionPool pool = null;
+    private static DataSource dataSource = null;
+
+    private ConnectionPool() {
+        try {
+            InitialContext ic = new InitialContext();
+            dataSource = (DataSource) ic.lookup("jdbc:mysql://database-1.c7l44jpgabmu.us-east-1.rds.amazonaws.com:3306/userdb");
+        } catch (NamingException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static synchronized ConnectionPool getInstance() {
+        if (pool == null) {
+            pool = new ConnectionPool();
+        }
+        
+        return pool;
+    }
+
+    public Connection getConnection() {
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    public void freeConnection(Connection c) {
+        try {
+            c.close();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+}
