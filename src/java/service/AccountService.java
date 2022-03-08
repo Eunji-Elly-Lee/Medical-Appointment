2c3cdf379e82753ce97c9af5e97ea6259eb9bc6f
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-03-07 23:55:33 -0700

    Update AccountService.java

--- src/java/service/AccountService.java
+++ src/java/service/AccountService.java
@@ -5,15 +5,29 @@ import java.util.List;
 import models.Account;
 
 public class AccountService {
+    public Account login(String user_name, String password) {
+        AccountDB accountDB = new AccountDB();
+        
+        try {
+            Account account = get(user_name);
+            
+            if(password.equals(account.getPassword())) {
+                return account;
+            }
+        } catch(Exception e) {}
+        
+        return null;
+    }
+    
     public List<Account> getAll() throws Exception {
         AccountDB accountDB = new AccountDB();
         List<Account> accounts = accountDB.getAll();
         return accounts;
     }
     
-    public Account get(int account_id) throws Exception {
+    public Account get(String user_name) throws Exception {
         AccountDB accountDB = new AccountDB();
-        Account account = accountDB.get(account_id);
+        Account account = accountDB.get(user_name);
         return account;
     }
     
@@ -29,8 +43,8 @@ public class AccountService {
         accountDB.update(account);
     }
     
-    public void delete(int account_id) throws Exception {
-        Account account = get(account_id);
+    public void delete(String user_name) throws Exception {
+        Account account = get(user_name);
         AccountDB accountDB = new AccountDB();
         accountDB.delete(account);
     }
