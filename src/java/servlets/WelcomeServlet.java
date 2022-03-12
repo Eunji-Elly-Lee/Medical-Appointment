636882ffdaa661bc0eaffbbf5ba9db27cb85bac7
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-03-12 00:15:59 -0700

    Update WelcomeServlet.java from jonghan

--- src/java/servlets/WelcomeServlet.java
+++ src/java/servlets/WelcomeServlet.java
@@ -1,6 +1,7 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.*;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
@@ -16,14 +17,15 @@ public class WelcomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        allPasswordEncrypted(request, response);        
         HttpSession session = request.getSession();
         String user_name = (String) session.getAttribute("user_name");
         
-        if(request.getParameter("logout") != null) {
+        if (request.getParameter("logout") != null) {
             session.invalidate();
             session = request.getSession();
         } else {
-            if(user_name != null && !user_name.equals("")) {
+            if (user_name != null && !user_name.equals("")) {
                 AccountService accountService = new AccountService();
 
                 try {
@@ -45,4 +47,24 @@ public class WelcomeServlet extends HttpServlet {
             throws ServletException, IOException {
        
     }
+    
+    public void allPasswordEncrypted(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        AccountService accountService = new AccountService();
+        
+        try {
+            List<Account> accounts = accountService.getAll();
+            
+            for (int i = 0; i < accounts.size(); i++) {
+                Account account = accounts.get(i);
+                
+                if (account.getSalt() == null) {                    
+                    accountService.update(account.getAccount_id(), account.getUser_name(),
+                            account.getPassword(), account.getProfile());
+                }
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }    
 }
