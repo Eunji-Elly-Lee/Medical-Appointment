1757420530b4e826cec915d07f5a74a710273884
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-03-11 22:22:12 -0700

    Update AccountDB.java

--- src/java/dataaccess/AccountDB.java
+++ src/java/dataaccess/AccountDB.java
@@ -2,8 +2,12 @@ package dataaccess;
 
 import java.sql.*;
 import java.util.*;
-import models.Account;
+import models.*;
 
+/**
+ *
+ * @author Kevin, Samia, Fied, Yisong, Jihoon, Jonghan, Elly
+ */
 public class AccountDB {
     public List<Account> getAll() throws Exception {
         List<Account> accounts = new ArrayList<>();
@@ -22,10 +26,10 @@ public class AccountDB {
                 String user_name = rs.getString(2);
                 String password = rs.getString(3);
                 String profile = rs.getString(4); 
-                String resetPasswordUuid = rs.getString(5);
+                String reset_password_uuid = rs.getString(5);
                 String salt = rs.getString(6);               
                 
-                Account account = new Account(account_id, user_name, password, profile, resetPasswordUuid, salt);
+                Account account = new Account(account_id, user_name, password, profile, reset_password_uuid, salt);
                 accounts.add(account);
             }
         } finally {
@@ -54,10 +58,10 @@ public class AccountDB {
                 int account_id = rs.getInt(1);
                 String password = rs.getString(3);
                 String profile = rs.getString(4);  
-                String resetPasswordUuid = rs.getString(5);  
+                String reset_password_uuid = rs.getString(5);  
                 String salt = rs.getString(6);  
                 
-                account = new Account(account_id, user_name, password, profile, resetPasswordUuid, salt);
+                account = new Account(account_id, user_name, password, profile, reset_password_uuid, salt);
             }
         } finally {
             DBUtil.closeResultSet(rs);
@@ -66,6 +70,71 @@ public class AccountDB {
         }
         
         return account;
+    }
+    
+    public Account get(int account_id) throws Exception {
+        Account account = null;
+        ConnectionPool cp = ConnectionPool.getInstance();
+        Connection con = cp.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        String sql = "SELECT * FROM account WHERE account_id = ?";
+        
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, account_id);
+            rs = ps.executeQuery();
+            
+            if(rs.next()) {
+                String user_name = rs.getString(2);
+                String password = rs.getString(3);
+                String profile = rs.getString(4);  
+                String reset_password_uuid = rs.getString(5);  
+                String salt = rs.getString(6);  
+                
+                account = new Account(account_id, user_name, password, profile, reset_password_uuid, salt);
+            }
+        } finally {
+            DBUtil.closeResultSet(rs);
+            DBUtil.closePreparedStatement(ps);
+            cp.freeConnection(con);
+        }
+        
+        return account;
+    }
+    
+    public Account getByEmail(String email) throws Exception {
+        AccountDB accountDB = new AccountDB();
+        Account account = new Account();
+        DoctorDB doctorDB = new DoctorDB();
+        PatientDB patientDB = new PatientDB();
+        AdministratorDB administratorDB = new AdministratorDB();
+
+        if (doctorDB.get(email) != null){
+           Doctor doctor = doctorDB.get(email);
+            account = accountDB.get(doctor.getAccount_id());
+        } else if (patientDB.get(email) != null){
+            Patient patient = patientDB.get(email);
+            account = accountDB.get(patient.getAccount_id());
+        } else if (administratorDB.get(email) != null){
+            Administrator admin = administratorDB.get(email);
+            account = accountDB.get(admin.getAccount_id());
+        } 
+        
+        return account;
+    }
+    
+    public Account getByUUID(String uuid) throws Exception {
+        AccountDB accountDB = new AccountDB();
+        List<Account> accounts = accountDB.getAll();
+        
+        for (int i=0; i < accounts.size(); i++){
+            if (accounts.get(i).getReset_password_uuid() != null && accounts.get(i).getReset_password_uuid().equals(uuid)) {
+                return accounts.get(i);
+            }
+        }
+        
+        return null;
     }
     
     public void insert(Account account) throws Exception {
@@ -82,7 +151,7 @@ public class AccountDB {
             ps.setString(2, account.getUser_name());
             ps.setString(3, account.getPassword());
             ps.setString(4, account.getProfile());  
-            ps.setString(5, account.getResetPasswordUuid());
+            ps.setString(5, account.getReset_password_uuid());
             ps.setString(6, account.getSalt());
             ps.executeUpdate();
         } finally {
@@ -104,7 +173,7 @@ public class AccountDB {
             ps.setString(2, account.getUser_name());
             ps.setString(3, account.getPassword());
             ps.setString(4, account.getProfile());
-            ps.setString(5, account.getResetPasswordUuid());
+            ps.setString(5, account.getReset_password_uuid());
             ps.setString(6, account.getSalt());
             ps.setInt(7, account.getAccount_id());
             ps.executeUpdate();
