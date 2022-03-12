4435bb2e967435d7044fec990e5abc54c2fb5ac3
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-03-11 22:38:37 -0700

    Update Administrator.java

--- src/java/models/Administrator.java
+++ src/java/models/Administrator.java
@@ -1,10 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
+/**
+ *
+ * @author Kevin, Samia, Fied, Yisong, Jihoon, Jonghan, Elly
+ */
 public class Administrator {
     private int admin_id;
     private String first_name;
@@ -21,7 +20,9 @@ public class Administrator {
     private String province;
     private String postal_code;
 
-    public Administrator(int admin_id, String first_name, String last_name, String email, String mobile_phone, String alt_phone, String pref_contact_type, int account_id, String gender, String birth_date, String street_address, String city, String province, String postal_code) {
+    public Administrator(int admin_id, String first_name, String last_name, String email,
+            String mobile_phone, String alt_phone, String pref_contact_type, int account_id, String gender,
+            String birth_date, String street_address, String city, String province, String postal_code) {
         this.admin_id = admin_id;
         this.first_name = first_name;
         this.last_name = last_name;
@@ -46,104 +47,100 @@ public class Administrator {
         return first_name;
     }
 
-    public String getLast_name() {
-        return last_name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getMobile_phone() {
-        return mobile_phone;
-    }
-
-    public String getAlt_phone() {
-        return alt_phone;
-    }
-
-    public String getPref_contact_type() {
-        return pref_contact_type;
-    }
-
-    public int getAccount_id() {
-        return account_id;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public String getBirth_date() {
-        return birth_date;
-    }
-
-    public String getStreet_address() {
-        return street_address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public String getProvince() {
-        return province;
-    }
-
-    public String getPostal_code() {
-        return postal_code;
-    }
-
-    public void setAdmin_id(int admin_id) {
-        this.admin_id = admin_id;
-    }
-
     public void setFirst_name(String first_name) {
         this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
     }
 
     public void setLast_name(String last_name) {
         this.last_name = last_name;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getMobile_phone() {
+        return mobile_phone;
     }
 
     public void setMobile_phone(String mobile_phone) {
         this.mobile_phone = mobile_phone;
     }
 
+    public String getAlt_phone() {
+        return alt_phone;
+    }
+
     public void setAlt_phone(String alt_phone) {
         this.alt_phone = alt_phone;
+    }
+
+    public String getPref_contact_type() {
+        return pref_contact_type;
     }
 
     public void setPref_contact_type(String pref_contact_type) {
         this.pref_contact_type = pref_contact_type;
     }
 
+    public int getAccount_id() {
+        return account_id;
+    }
+
     public void setAccount_id(int account_id) {
         this.account_id = account_id;
+    }
+
+    public String getGender() {
+        return gender;
     }
 
     public void setGender(String gender) {
         this.gender = gender;
     }
 
+    public String getBirth_date() {
+        return birth_date;
+    }
+
     public void setBirth_date(String birth_date) {
         this.birth_date = birth_date;
+    }
+
+    public String getStreet_address() {
+        return street_address;
     }
 
     public void setStreet_address(String street_address) {
         this.street_address = street_address;
     }
 
+    public String getCity() {
+        return city;
+    }
+
     public void setCity(String city) {
         this.city = city;
     }
 
+    public String getProvince() {
+        return province;
+    }
+
     public void setProvince(String province) {
         this.province = province;
+    }
+
+    public String getPostal_code() {
+        return postal_code;
     }
 
     public void setPostal_code(String postal_code) {
@@ -152,9 +149,21 @@ public class Administrator {
 
     @Override
     public String toString() {
-        return "Administrator{" + "admin_id=" + admin_id + ", first_name=" + first_name + ", last_name=" + last_name + ", email=" + email + ", mobile_phone=" + mobile_phone + ", alt_phone=" + alt_phone + ", pref_contact_type=" + pref_contact_type + ", account_id=" + account_id + ", gender=" + gender + ", birth_date=" + birth_date + ", street_address=" + street_address + ", city=" + city + ", province=" + province + ", postal_code=" + postal_code + '}';
+        return "Administrator{" +
+                "admin_id=" + admin_id +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", email='" + email + '\'' +
+                ", mobile_phone='" + mobile_phone + '\'' +
+                ", alt_phone='" + alt_phone + '\'' +
+                ", pref_contact_type='" + pref_contact_type + '\'' +
+                ", account_id=" + account_id +
+                ", gender='" + gender + '\'' +
+                ", birth_date='" + birth_date + '\'' +
+                ", street_address='" + street_address + '\'' +
+                ", city='" + city + '\'' +
+                ", province='" + province + '\'' +
+                ", postal_code='" + postal_code + '\'' +
+                '}';
     }
-    
-     
-    
 }
