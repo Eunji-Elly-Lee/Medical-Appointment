d325c079763c5930d02461129416b1b98780b9f0
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-03-12 16:23:56 -0700

    Update ProfileServlet.java

--- src/java/servlets/ProfileServlet.java
+++ src/java/servlets/ProfileServlet.java
@@ -7,77 +7,124 @@ import javax.servlet.http.*;
 import models.*;
 import service.*;
 
+/**
+ *
+ * @author Kevin, Samia, Fied, Yisong, Jihoon, Jonghan, Elly
+ */
 public class ProfileServlet extends HttpServlet {
+    
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         HttpSession session = request.getSession();
-        String user_name = (String)session.getAttribute("user_name");        
-        AccountService accountService = new AccountService();
-        DoctorService doctorService = new DoctorService();
-        PatientService patientService = new PatientService();
+        String user_name = (String)session.getAttribute("user_name");
         
-        Account account = null;
-        try {
-            account = accountService.get(user_name);
-            if (account.getProfile().equals("DOCTOR")) {
-                Doctor doctor = doctorService.get(account.getAccount_id());
-
+        if (user_name == null || user_name.equals("")) {
+            response.sendRedirect("welcome");
+            return;
+        } else {
+            AccountService accountService = new AccountService();
+            
+            try {
+                Account account = accountService.get(user_name);
                 request.setAttribute("account", account);
-                request.setAttribute("doctor", doctor);
-                getServletContext().getRequestDispatcher("/WEB-INF/doctorProfile.jsp").forward(request, response);
-            } else if (account.getProfile().equals("PATIENT")) {
-                Patient patient = patientService.get(account.getAccount_id());
-                request.setAttribute("account", account);
-                request.setAttribute("patient", patient);
-                System.out.println(patient.getPref_contact_type());
-                getServletContext().getRequestDispatcher("/WEB-INF/patientProfile.jsp").forward(request, response);
-
+                request.setAttribute("login", "login");
+                
+                if (account.getProfile().equals("DOCTOR")) {
+                    DoctorService doctorService = new DoctorService();
+                    Doctor doctor = doctorService.get(account.getAccount_id());
+                    request.setAttribute("user", doctor);
+                } else if (account.getProfile().equals("ADMIN") || account.getProfile().equals("SYSADMIN")) {
+                    AdministratorService administratorService = new AdministratorService();
+                    Administrator administrator = administratorService.get(account.getAccount_id());
+                    request.setAttribute("user", administrator);
+                } else if (account.getProfile().equals("PATIENT")) {
+                    PatientService patientService = new PatientService();
+                    Patient patient = patientService.get(account.getAccount_id());
+                    request.setAttribute("user", patient);
+                }
+            } catch(Exception ex) {
+                    Logger.getLogger(WelcomeServlet.class.getName()).log(Level.SEVERE, null, ex);
             }
-
-        } catch(Exception ex) {
-            Logger.getLogger(ProfileServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        getServletContext().getRequestDispatcher("/WEB-INF/doctorProfile.jsp").forward(request, response);
+        getServletContext().getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+        return;
     }
     
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         HttpSession session = request.getSession();
         String user_name = (String)session.getAttribute("user_name");
+        String first_name = request.getParameter("first_name");
+        String last_name = request.getParameter("last_name");
         String password = request.getParameter("password");
         String repassword = request.getParameter("repassword");
         String phone_number = request.getParameter("phone_number");
-        String alter_Phone_number = request.getParameter("alter_Phone_number");
+        String alt_phone = request.getParameter("alt_phone");
         String email = request.getParameter("email");
         String pref_contact_type = request.getParameter("pref_contact_type");
+        String gender = request.getParameter("gender");
+        String birth_date = request.getParameter("birth_date");
         String street_address = request.getParameter("street_address");
+        String city = request.getParameter("city");
         String postal_code = request.getParameter("postal_code");
+        String province = request.getParameter("province");
         
-        AccountService accountService = new AccountService();
-        DoctorService doctorService = new DoctorService();
+        if (first_name == null || first_name.equals("") || last_name == null || last_name.equals("") ||
+                phone_number == null || phone_number.equals("") || email == null || email.equals("") ||
+                birth_date == null || birth_date.equals("") || street_address == null || street_address.equals("") ||
+                city == null || city.equals("") || postal_code == null || postal_code.equals("") ||
+                province == null || province.equals("")) {
+            request.setAttribute("message", "Please fill out all the required information.");        
+        } else {
+            AccountService accountService = new AccountService();
         
-        try {
-            Account account = accountService.get(user_name);
-            Doctor doctor = doctorService.get(account.getAccount_id());
-            
-            if(password != null && !password.equals("") && password.equals(repassword)) {
-                accountService.update(account.getAccount_id(), user_name, password, account.getProfile()); 
+            try {
+                Account account = accountService.get(user_name);
+
+                if(password != null && !password.equals("") && password.equals(repassword)) {
+                    accountService.update(account.getAccount_id(), user_name, password, account.getProfile()); 
+                }
+
+                if (account.getProfile().equals("DOCTOR")) {
+                    DoctorService doctorService = new DoctorService();
+                    Doctor doctor = doctorService.get(account.getAccount_id());
+                    doctorService.update(doctor.getDoctor_id(), first_name, last_name, email,
+                        phone_number, alt_phone, pref_contact_type, doctor.getAccount_id(), gender,
+                        birth_date, street_address, city, province, postal_code);
+                    
+                    Doctor updatedDoctor = doctorService.get(account.getAccount_id());
+                    request.setAttribute("user", updatedDoctor);
+                } else if (account.getProfile().equals("ADMIN") || account.getProfile().equals("SYSADMIN")) {
+                    AdministratorService administratorService = new AdministratorService();
+                    Administrator administrator = administratorService.get(account.getAccount_id());
+                    administratorService.update(administrator.getAdmin_id(), first_name, last_name, email,
+                        phone_number, alt_phone, pref_contact_type, administrator.getAccount_id(), gender,
+                        birth_date, street_address, city, province, postal_code);
+                    
+                    Administrator updatedAdministrator = administratorService.get(account.getAccount_id());
+                    request.setAttribute("user", updatedAdministrator);
+                } else if (account.getProfile().equals("PATIENT")) {
+                    PatientService patientService = new PatientService();
+                    Patient patient = patientService.get(account.getAccount_id());
+                    patientService.update(patient.getPatient_id(), patient.getHealthcare_id(), first_name, last_name, email,
+                        phone_number, alt_phone, pref_contact_type, patient.getDoctor_id(), patient.getAccount_id(), gender,
+                        birth_date, street_address, city, province, postal_code);
+                    
+                    Patient updatedPatient = patientService.get(account.getAccount_id());
+                    request.setAttribute("user", updatedPatient);
+                }
+
+                Account updatedAccount = accountService.get(user_name);
+                request.setAttribute("account", updatedAccount);
+            } catch(Exception ex) {
+                Logger.getLogger(ProfileServlet.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
-            doctorService.update(doctor.getDoctor_id(), doctor.getFirst_name(), doctor.getLast_name(), email,
-                    phone_number, alter_Phone_number, pref_contact_type, doctor.getAccount_id(), doctor.getGender(),
-                    doctor.getBirth_date(), street_address, doctor.getCity(), doctor.getProvince(), postal_code);
-            
-            Account updatedAccount = accountService.get(user_name);
-            Doctor updatedDoctor = doctorService.get(account.getAccount_id());
-            
-            request.setAttribute("account", updatedAccount);
-            request.setAttribute("doctor", updatedDoctor);
-        } catch(Exception ex) {
-            Logger.getLogger(ProfileServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
         
         getServletContext().getRequestDispatcher("/WEB-INF/doctorProfile.jsp").forward(request, response);
+        return;
     }
 }
