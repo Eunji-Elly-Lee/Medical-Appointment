2d37c151e23fb771f578883bc9045bfb8df23be6
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-03-13 15:26:23 -0600

    Update Calendar.java

--- src/java/models/Calendar.java
+++ src/java/models/Calendar.java
@@ -5,38 +5,34 @@ package models;
  * @author Kevin, Samia, Fied, Yisong, Jihoon, Jonghan, Elly
  */
 public class Calendar {
-    private String date;
-    private String time;
+    private String date_time;
     private String clinic_open;
 
-
-    public Calendar(String date, String time, String clinic_open) {
-        this.date = date;
-        this.time = time;
+    public Calendar() {
+    }
+    
+    public Calendar(String date_time, String clinic_open) {
+        this.date_time = date_time;
         this.clinic_open = clinic_open;
     }
 
-    public String getDate() {
-        return date;
-    }
-
-    public String getTime() {
-        return time;
+    public String getDate_time() {
+        return date_time;
     }
     
     public String getClinic_open() {
         return clinic_open;
     }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
     
     public void setClinic_open(String clinic_open) {
         this.clinic_open = clinic_open;
+    }
+    
+    @Override
+    public String toString() {
+        return "Calendar{" +
+                "date_time=" + date_time +
+                ", clinic_open='" + clinic_open + '\'' +
+                '}';
     }
 }
