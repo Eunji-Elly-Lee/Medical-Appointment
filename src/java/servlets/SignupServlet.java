178075da0e4b9a6d36b2f7228788a2b258d02df4
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-03-13 01:43:56 -0700

    Update SignupServlet.java

--- src/java/servlets/SignupServlet.java
+++ src/java/servlets/SignupServlet.java
@@ -1,20 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 
 /**
  *
- * @author ADMIN
+ * @author Kevin, Samia, Fied, Yisong, Jihoon, Jonghan, Elly
  */
 public class SignupServlet extends HttpServlet {
 
@@ -28,7 +20,5 @@ public class SignupServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
     }
-
 }
