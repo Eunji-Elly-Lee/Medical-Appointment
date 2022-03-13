d5215cea27cba73c207606d4438f2472f20d2d3e
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-03-13 10:46:06 -0600

    Update ViewAppointmentServlet.java

--- src/java/servlets/ViewAppointmentServlet.java
+++ src/java/servlets/ViewAppointmentServlet.java
@@ -14,6 +14,7 @@ public class ViewAppointmentServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
           getServletContext().getRequestDispatcher("/WEB-INF/viewAppointment.jsp").forward(request, response);
+          return;
     }
   
     @Override
