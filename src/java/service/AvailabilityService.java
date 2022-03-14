288415f5af34eb91e867ab09fe26e69739c16610
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-03-13 19:23:38 -0600

    Create AvailabilityService.java

---
+++ src/java/service/AvailabilityService.java
@@ -0,0 +1,41 @@
+package service;
+
+import dataaccess.AvailabilityDB;
+import java.util.List;
+import models.Availability;
+
+/**
+ *
+ * @author Kevin, Samia, Fied, Yisong, Jihoon, Jonghan, Elly
+ */
+public class AvailabilityService {
+    public List<Availability> getAll() throws Exception {
+        AvailabilityDB availabilityDB = new AvailabilityDB();
+        List<Availability> availabilities = availabilityDB.getAll();
+        return availabilities;
+    }
+    
+    public Availability get(int doctor_id) throws Exception {
+        AvailabilityDB availabilityDB = new AvailabilityDB();
+        Availability availability = availabilityDB.get(doctor_id);
+        return availability;
+    }
+    
+    public void insert(int doctor_id, String start_date_time, int duration) throws Exception {
+        Availability availability = new Availability(doctor_id , start_date_time, duration);
+        AvailabilityDB availabilityDB = new AvailabilityDB();
+        availabilityDB.insert(availability);
+    }
+    
+    public void update(int doctor_id, String start_date_time, int duration) throws Exception {
+        Availability availability = new Availability(doctor_id , start_date_time, duration);
+        AvailabilityDB availabilityDB = new AvailabilityDB();
+        availabilityDB.update(availability);
+    }
+    
+    public void delete(int doctor_id) throws Exception {
+        Availability availability = get(doctor_id);
+        AvailabilityDB availabilityDB = new AvailabilityDB();
+        availabilityDB.delete(availability);
+    }
+}
