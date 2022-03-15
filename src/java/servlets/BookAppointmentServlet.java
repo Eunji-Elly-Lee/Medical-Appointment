7678e2e9acb26bd2fb09c79c76319149325074bd
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-03-14 18:31:00 -0600

    Update BookAppointmentServlet.java

--- src/java/servlets/BookAppointmentServlet.java
+++ src/java/servlets/BookAppointmentServlet.java
@@ -13,12 +13,15 @@ import models.Account;
 import models.Administrator;
 import models.AppointmentType;
 import models.Availability;
+import models.Calendar;
 import models.Doctor;
 import models.Patient;
 import service.AccountService;
 import service.AdministratorService;
+import service.AppointmentService;
 import service.AppointmentTypeService;
 import service.AvailabilityService;
+import service.CalendarService;
 import service.DoctorService;
 import service.PatientService;
 
@@ -43,7 +46,49 @@ public class BookAppointmentServlet extends HttpServlet {
                 request.setAttribute("step", "1");
             }
             
-            displayInformation(request, user_name);
+            AccountService accountService = new AccountService();
+            DoctorService doctorService = new DoctorService();
+            PatientService patientService = new PatientService();
+            AvailabilityService availabilityService = new AvailabilityService();
+            
+            List<Availability> availabilities = new ArrayList<>();
+            LocalDate tomorrow = LocalDate.now().plusDays(1);
+            // for test version!
+            tomorrow = tomorrow.withMonth(1);
+            
+            try {
+                Account account = accountService.get(user_name);
+                request.setAttribute("account", account);
+
+                if (account.getProfile().equals("DOCTOR")) {                    
+                    Doctor doctor = doctorService.get(account.getAccount_id());
+                    request.setAttribute("user", doctor);
+                } else if (account.getProfile().equals("ADMIN") || account.getProfile().equals("SYSADMIN")) {
+                    AdministratorService administratorService = new AdministratorService();
+                    Administrator administrator = administratorService.get(account.getAccount_id());
+                    request.setAttribute("user", administrator);
+                } else if (account.getProfile().equals("PATIENT")) {                    
+                    Patient patient = patientService.get(account.getAccount_id());
+                    Doctor doctor = doctorService.getByDoctorID(patient.getDoctor_id());
+                    
+                    availabilities =
+                            availabilityService.getAllByDoctorDate(doctor.getDoctor_id(), tomorrow.toString());
+                    
+                    if (!availabilities.isEmpty()) {
+                        List<String> available_dates = new ArrayList<>();
+
+                        for (Availability availability: availabilities) {
+                            available_dates.add(availability.getStart_date_time().substring(0, 11));
+                        }
+
+                        request.setAttribute("available_dates", available_dates);
+                    }
+                    
+                    request.setAttribute("user", patient);
+                }
+            } catch (Exception ex) {
+                    Logger.getLogger(WelcomeServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
         
         getServletContext().getRequestDispatcher("/WEB-INF/bookAppointment.jsp").forward(request, response);
@@ -55,78 +100,44 @@ public class BookAppointmentServlet extends HttpServlet {
             throws ServletException, IOException {
         HttpSession session = request.getSession();
         String user_name = (String) session.getAttribute("user_name");       
-        
-        List<Availability> availabilities = displayInformation(request, user_name);
         String appointment_date = request.getParameter("appointment_date");
         String action = request.getParameter("action");
         
-        switch (action) {
-                case "select_date":
-                    String end_date_time = null;
-                    String duration = null;
-                    
-                    for (Availability availability: availabilities) {
-                        if (availability.getStart_date_time().startsWith(appointment_date)) {                            
-                            end_date_time = availability.getStart_date_time().substring(0, 19);
-                            duration = availability.getDuration() + "";                            
-                        }
-                    }
-                    
-                    LocalTime time = LocalTime.of(Integer.parseInt(end_date_time.substring(11, 13)),
-                            Integer.parseInt(end_date_time.substring(14, 16)), Integer.parseInt(end_date_time.substring(17)));
-                    
-                    time = time.plusMinutes(Integer.parseInt(duration));
-                    
-                    end_date_time = end_date_time.substring(0, 11) + time;
-                    
-//                    System.out.println("appointment_date: " + appointment_date);
-//                    System.out.println("end_date_time: " + end_date_time);
-                    
-                    request.setAttribute("appointment_date", appointment_date);
-                    break;
-                
-                case "book_appointment":
-                    request.setAttribute("step", "1");
-                    break;
-        }
-        
-        getServletContext().getRequestDispatcher("/WEB-INF/bookAppointment.jsp").forward(request, response);
-        return;
-    }
-    
-    private List<Availability> displayInformation(HttpServletRequest request, String user_name) {
-        AccountService accountService = new AccountService();
-        AdministratorService administratorService = new AdministratorService();
-        DoctorService doctorService = new DoctorService();
-        PatientService patientService = new PatientService();
-        AppointmentTypeService appointmentTypeService = new AppointmentTypeService();
-        AvailabilityService availabilityService = new AvailabilityService();
-        
+        List<AppointmentType> types = new ArrayList<>();
         List<Availability> availabilities = new ArrayList<>();
         LocalDate tomorrow = LocalDate.now().plusDays(1);
         // for test version!
         tomorrow = tomorrow.withMonth(1);
         
-
+        AccountService accountService = new AccountService();        
+        DoctorService doctorService = new DoctorService();
+        PatientService patientService = new PatientService();
+        AppointmentService appointmentService = new AppointmentService();
+        AppointmentTypeService appointmentTypeService = new AppointmentTypeService();
+        AvailabilityService availabilityService = new AvailabilityService();
+        CalendarService calendarService = new CalendarService();
+        
+        Doctor doctor = null;
+        Patient patient = null;
+        
         try {
             Account account = accountService.get(user_name);
-            List<AppointmentType> types = new ArrayList<>();
-            
             request.setAttribute("account", account);
                 
             if (account.getProfile().equals("DOCTOR")) {
-                Doctor doctor = doctorService.get(account.getAccount_id());
+                doctor = doctorService.get(account.getAccount_id());
                 request.setAttribute("user", doctor);
-            } else if (account.getProfile().equals("ADMIN") || account.getProfile().equals("SYSADMIN")) {                
+            } else if (account.getProfile().equals("ADMIN") || account.getProfile().equals("SYSADMIN")) {
+                AdministratorService administratorService = new AdministratorService();
                 Administrator administrator = administratorService.get(account.getAccount_id());
                 request.setAttribute("user", administrator);
             } else if (account.getProfile().equals("PATIENT")) {                
-                Patient patient = patientService.get(account.getAccount_id());    
+                patient = patientService.get(account.getAccount_id());    
                 
                 if (patient.getDoctor_id() == 0) {
                     types.add(appointmentTypeService.get(4));
                 } else {
-                    Doctor doctor = doctorService.getByDoctorID(patient.getDoctor_id());                   
+                    doctor = doctorService.getByDoctorID(patient.getDoctor_id());                   
                     request.setAttribute("doctor", doctor);
                     types.add(appointmentTypeService.get(1));
                     types.add(appointmentTypeService.get(2));   
@@ -142,9 +153,6 @@ public class BookAppointmentServlet extends HttpServlet {
                         }
                         
                         request.setAttribute("available_dates", available_dates);
-//                        for (String date: available_dates) {
-//                            System.out.println("date: " + date); 
-//                        }
                     }
                 }
                 
@@ -152,11 +160,46 @@ public class BookAppointmentServlet extends HttpServlet {
 //                request.setAttribute("message", "You new");
             }
             
+            switch (action) {
+                case "select_date":
+                    String start_date_time = null;
+                    String end_date_time = null;
+                    String duration = null;
+                    
+                    for (Availability availability: availabilities) {
+                        if (availability.getStart_date_time().startsWith(appointment_date)) {
+                            start_date_time = availability.getStart_date_time().substring(0, 19);
+                            end_date_time = start_date_time;
+                            duration = availability.getDuration() + "";                            
+                        }
+                    }
+                    
+                    LocalTime end_time = LocalTime.of(Integer.parseInt(end_date_time.substring(11, 13)),
+                            Integer.parseInt(end_date_time.substring(14, 16)), Integer.parseInt(end_date_time.substring(17)));                    
+                    end_time = end_time.plusMinutes(Integer.parseInt(duration));                    
+                    end_date_time = end_date_time.substring(0, 11) + end_time;
+                    
+                    List<Calendar> calendars = calendarService.getAllAvailable(start_date_time, end_date_time, doctor.getDoctor_id());
+                    List<String> available_times = new ArrayList<>();
+                    
+                    
+//                    System.out.println("start_date_time: " + start_date_time);
+//                    System.out.println("end_date_time: " + end_date_time);
+                    
+                    request.setAttribute("appointment_date", appointment_date);
+                    break;
+                
+                case "book_appointment":
+                    request.setAttribute("step", "1");
+                    break;
+        }
+            
             request.setAttribute("types", types);
         } catch (Exception ex) {
                 Logger.getLogger(WelcomeServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        return availabilities;
+        getServletContext().getRequestDispatcher("/WEB-INF/bookAppointment.jsp").forward(request, response);
+        return;
     }
 }
