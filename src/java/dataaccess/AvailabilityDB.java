3ebb4710b8e89d50414b1155cd501ac4ec738ebc
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-03-27 22:15:02 -0600

    Update AvailabilityDB.java from Jihoon

--- src/java/dataaccess/AvailabilityDB.java
+++ src/java/dataaccess/AvailabilityDB.java
@@ -14,18 +14,18 @@ public class AvailabilityDB {
         ConnectionPool cp = ConnectionPool.getInstance();
         Connection con = cp.getConnection();
         PreparedStatement ps = null;
-        ResultSet rs = null;        
+        ResultSet rs = null;
         String sql = "SELECT * FROM availability";
-        
+
         try {
             ps = con.prepareStatement(sql);
             rs = ps.executeQuery();
-            
+
             while (rs.next()) {
                 int doctor_id = rs.getInt(1);
                 String start_date_time = rs.getString(2);
-                int duration = rs.getInt(3);               
-                
+                int duration = rs.getInt(3);
+
                 Availability availability = new Availability(doctor_id, start_date_time, duration);
                 availabilities.add(availability);
             }
@@ -34,28 +34,28 @@ public class AvailabilityDB {
             DBUtil.closePreparedStatement(ps);
             cp.freeConnection(con);
         }
-        
+
         return availabilities;
     }
-    
+
     public List<Availability> getAllByDoctorDate(int doctor_id, String start_date_time) throws Exception {
         List<Availability> availabilities = new ArrayList<>();
         ConnectionPool cp = ConnectionPool.getInstance();
         Connection con = cp.getConnection();
         PreparedStatement ps = null;
-        ResultSet rs = null;        
+        ResultSet rs = null;
         String sql = "SELECT * FROM availability where doctor_id = ? and start_date_time > ?";
-        
+
         try {
             ps = con.prepareStatement(sql);
             ps.setInt(1, doctor_id);
             ps.setString(2, start_date_time);
             rs = ps.executeQuery();
-            
+
             while (rs.next()) {
                 start_date_time = rs.getString(2);
-                int duration = rs.getInt(3);               
-                
+                int duration = rs.getInt(3);
+
                 Availability availability = new Availability(doctor_id, start_date_time, duration);
                 availabilities.add(availability);
             }
@@ -64,10 +64,39 @@ public class AvailabilityDB {
             DBUtil.closePreparedStatement(ps);
             cp.freeConnection(con);
         }
-        
+
         return availabilities;
     }
-    
+
+    // add for doctor schedule
+    public List<Availability> getAllByDoctorId(int doctor_id) throws Exception {
+        List<Availability> availabilities = new ArrayList<>();
+        ConnectionPool cp = ConnectionPool.getInstance();
+        Connection con = cp.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        String sql = "SELECT * FROM availability where doctor_id = ?";
+
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, doctor_id);
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                String start_date_time = rs.getString(2);
+                int duration = rs.getInt(3);
+                Availability availability = new Availability(doctor_id, start_date_time, duration);
+                availabilities.add(availability);
+            }
+        } finally {
+            DBUtil.closeResultSet(rs);
+            DBUtil.closePreparedStatement(ps);
+            cp.freeConnection(con);
+        }
+
+        return availabilities;
+    }
+
     public Availability get(int doctor_id) throws Exception {
         Availability availability = null;
         ConnectionPool cp = ConnectionPool.getInstance();
@@ -75,34 +104,34 @@ public class AvailabilityDB {
         PreparedStatement ps = null;
         ResultSet rs = null;
         String sql = "SELECT * FROM availability WHERE doctor_id = ?";
-        
+
         try {
             ps = con.prepareStatement(sql);
             ps.setInt(1, doctor_id);
             rs = ps.executeQuery();
-            
+
             if (rs.next()) {
                 String start_date_time = rs.getString(2);
                 int duration = rs.getInt(3);
-                
-                availability = new Availability(doctor_id , start_date_time, duration);
+
+                availability = new Availability(doctor_id, start_date_time, duration);
             }
         } finally {
             DBUtil.closeResultSet(rs);
             DBUtil.closePreparedStatement(ps);
             cp.freeConnection(con);
         }
-        
+
         return availability;
     }
-    
+
     public void insert(Availability availability) throws Exception {
         ConnectionPool cp = ConnectionPool.getInstance();
         Connection con = cp.getConnection();
         PreparedStatement ps = null;
         String sql = "INSERT INTO availability "
                 + "(doctor_id , start_date_time, duration) VALUES (?, ?, ?)";
-        
+
         try {
             ps = con.prepareStatement(sql);
             ps.setInt(1, availability.getDoctor_id());
@@ -114,14 +143,14 @@ public class AvailabilityDB {
             cp.freeConnection(con);
         }
     }
-    
+
     public void update(Availability availability) throws Exception {
         ConnectionPool cp = ConnectionPool.getInstance();
         Connection con = cp.getConnection();
         PreparedStatement ps = null;
-        String sql = "UPDATE availability SET doctor_id = ?, start_date_time = ?, " +
-                "duration = ? WHERE doctor_id = ?";
-        
+        String sql = "UPDATE availability SET doctor_id = ?, start_date_time = ?, "
+                + "duration = ? WHERE doctor_id = ?";
+
         try {
             ps = con.prepareStatement(sql);
             ps.setInt(1, availability.getDoctor_id());
@@ -134,13 +163,13 @@ public class AvailabilityDB {
             cp.freeConnection(con);
         }
     }
-    
+
     public void delete(Availability availability) throws Exception {
         ConnectionPool cp = ConnectionPool.getInstance();
         Connection con = cp.getConnection();
         PreparedStatement ps = null;
         String sql = "DELETE FROM availability WHERE doctor_id = ?";
-        
+
         try {
             ps = con.prepareStatement(sql);
             ps.setInt(1, availability.getDoctor_id());
