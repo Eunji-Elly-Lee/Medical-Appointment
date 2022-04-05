2bae4b252bb92ac573dbbefe764569d67209ea0e
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-04-05 17:05:50 -0600

    Update ViewPatientServlet.java

--- src/java/servlets/ViewPatientServlet.java
+++ src/java/servlets/ViewPatientServlet.java
@@ -35,35 +35,32 @@ public class ViewPatientServlet extends HttpServlet {
             request.setAttribute("message", "Please enter the name");
             session.setAttribute("noName", null);
         }
+        
+        if (user_name != null && !user_name.equals("")) {
+            AccountService accountService = new AccountService();
 
-        if (request.getParameter("logout") != null) {
-            session.invalidate();
-            session = request.getSession();
-        } else {
-            if (user_name != null && !user_name.equals("")) {
-                AccountService accountService = new AccountService();
+            try {
+                Account account = accountService.get(user_name);
+                request.setAttribute("account", account);
 
-                try {
-                    Account account = accountService.get(user_name);
-                    request.setAttribute("account", account);
-
-                    if (account.getProfile().equals("DOCTOR")) {
-                        DoctorService doctorService = new DoctorService();
-                        Doctor doctor = doctorService.get(account.getAccount_id());
-                        request.setAttribute("user", doctor);
-                    } else if (account.getProfile().equals("ADMIN") || account.getProfile().equals("SYSADMIN")) {
-                        AdministratorService administratorService = new AdministratorService();
-                        Administrator administrator = administratorService.get(account.getAccount_id());
-                        request.setAttribute("user", administrator);
-                    } else if (account.getProfile().equals("PATIENT")) {
-                        PatientService patientService = new PatientService();
-                        Patient patient = patientService.get(account.getAccount_id());
-                        request.setAttribute("user", patient);
-                    }
-                } catch (Exception ex) {
-                    Logger.getLogger(WelcomeServlet.class.getName()).log(Level.SEVERE, null, ex);
+                if (account.getProfile().equals("DOCTOR")) {
+                    DoctorService doctorService = new DoctorService();
+                    Doctor doctor = doctorService.get(account.getAccount_id());
+                    request.setAttribute("user", doctor);
+                } else if (account.getProfile().equals("ADMIN")) {
+                    AdministratorService administratorService = new AdministratorService();
+                    Administrator administrator = administratorService.get(account.getAccount_id());
+                    request.setAttribute("user", administrator);
+                } else {
+                    response.sendRedirect("welcome");
+                    return;
                 }
+            } catch (Exception ex) {
+                Logger.getLogger(WelcomeServlet.class.getName()).log(Level.SEVERE, null, ex);
             }
+        } else {
+            response.sendRedirect("welcome");
+            return;
         }
 
         getServletContext().getRequestDispatcher("/WEB-INF/viewPatient.jsp").forward(request, response);
