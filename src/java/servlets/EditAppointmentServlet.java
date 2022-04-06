f440539b0e23973f9d84bbe3917d7d458db04c53
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-04-06 09:05:55 -0600

    Update EditAppointmentServlet.java

--- src/java/servlets/EditAppointmentServlet.java
+++ src/java/servlets/EditAppointmentServlet.java
@@ -48,11 +48,9 @@ public class EditAppointmentServlet extends HttpServlet {
                 String patient = patientService.getByPatientId(appointSession.getPatient_id()).getFirst_name() +
                         " " + patientService.getByPatientId(appointSession.getPatient_id()).getLast_name();
                 
-                request.setAttribute("date", appointSession.getStart_date_time().substring(0, 16));
+                request.setAttribute("appointment", appointSession);
                 request.setAttribute("doctor", doctor);
                 request.setAttribute("patient", patient);
-                request.setAttribute("type", appointSession.getType());
-                request.setAttribute("reason", appointSession.getReason());
             } catch (Exception ex) {
                 Logger.getLogger(EditAppointmentServlet.class.getName()).log(Level.SEVERE, null, ex);
             }
@@ -66,75 +64,54 @@ public class EditAppointmentServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         HttpSession session = request.getSession();
-        DoctorService d = new DoctorService();
-
-        AppointmentService a = new AppointmentService();
-        String date = request.getParameter("dateonly");
-        String time = request.getParameter("timeonly");
-        String dID = request.getParameter("doctor_id");
-
-        String Datetime = date + ' ' + time;
-        int typeInt = 0;
-        int type = 0;
-        int duration = 0;
-        int doctorID = 0;
+        String user_name = (String) session.getAttribute("user_name");
+        Appointment appointSession = (Appointment) session.getAttribute("appointmentSessionObj");
+        String type = request.getParameter("type");
+        String reason = request.getParameter("reason");
         
+        AccountService as = new AccountService();
+        DoctorService doctorService = new DoctorService();
+        PatientService patientService = new PatientService();
+        AppointmentService aps = new AppointmentService();
+        AppointmentTypeService apts = new AppointmentTypeService();
+                        
         try {
-            type = Integer.parseInt(request.getParameter("form-select"));
-
-        } catch (NumberFormatException e) {
-        }
-
-        String reason = request.getParameter("book_app_reason");
-
-        if (type == 1) {
-            duration = 15;
-        } else if (type == 2) {
-            duration = 45;
-        } else if (type == 3) {
-            duration = 45;
-        } else if (type == 4) {
-            duration = 30;
-        }
-        
-        try {
-            String user_name = (String) session.getAttribute("user_name");
-
-            AccountService as = new AccountService();
             Account account = as.get(user_name);
-
-            Appointment appointSession = (Appointment) session.getAttribute("appointmentSessionObj");
-            Doctor doctorSession = (Doctor) session.getAttribute("doctorSessionObj");
-            Patient patientSession = (Patient) session.getAttribute("patientSessionObj");
-            Appointment appointment = null;
-            String startdate = null;
-
-            startdate = appointSession.getStart_date_time();
-            appointment = (Appointment) a.getByDate(startdate);
-            int docID = appointSession.getDoctor_id();
-
-            //      Doctor doctor = (Doctor) d.getByDoctorID(doctorID);
-            a.update(docID, startdate, appointment.getPatient_id(), duration, type, reason, appointment.getPatient_attended());
-
-            Appointment updated =
-                    new Appointment(docID, startdate, appointment.getPatient_id(), duration, type, reason, appointment.getPatient_attended());
-            request.setAttribute("message", "Successfully edited");
-            request.setAttribute("appointment", updated);
-            request.setAttribute("doctor", doctorSession);
-            request.setAttribute("patient", patientSession);
-
-            if (account.getProfile().equals("PATIENT")) {
-                getServletContext().getRequestDispatcher("/WEB-INF/editAppointmentPatient.jsp").forward(request, response);
-                return;
+            request.setAttribute("account", account);
+                
+            if (account.getProfile().equals("DOCTOR")) {                    
+                Doctor doctor = doctorService.get(account.getAccount_id());
+                request.setAttribute("user", doctor);
             } else if (account.getProfile().equals("ADMIN")) {
-                getServletContext().getRequestDispatcher("/WEB-INF/editAppointmentAdmin.jsp").forward(request, response);
-                return;
-            } else if (account.getProfile().equals("DOCTOR")) {
-                getServletContext().getRequestDispatcher("/WEB-INF/editAppointmentDoctor.jsp").forward(request, response);
-                return;
+                AdministratorService administratorService = new AdministratorService();
+                Administrator administrator = administratorService.get(account.getAccount_id());
+                request.setAttribute("user", administrator);
+            } else {
+                Patient patient = patientService.get(account.getAccount_id());
+                request.setAttribute("user", patient);
             }
-
-            //  response.sendRedirect("viewAppointmentPatient");
+                
+            String doctor = doctorService.getByDoctorID(appointSession.getDoctor_id()).getLast_name();
+            String patient = patientService.getByPatientId(appointSession.getPatient_id()).getFirst_name() +
+                    " " + patientService.getByPatientId(appointSession.getPatient_id()).getLast_name();
+              
+            request.setAttribute("appointment", appointSession);
+            request.setAttribute("doctor", doctor);
+            request.setAttribute("patient", patient);
+            
+            if (reason == null || reason.equals("")) {
+                request.setAttribute("message", "Please fill out the reason.");
+                getServletContext().getRequestDispatcher("/WEB-INF/editAppointment.jsp").forward(request, response);
+                return;
+            } else {
+                aps.update(appointSession.getDoctor_id(), appointSession.getStart_date_time(), appointSession.getPatient_id(),
+                        apts.get(Integer.parseInt(type)).getStd_duration(), Integer.parseInt(type), reason, false);
+                
+                session.setAttribute("appointmentSessionObj", null);
+                session.setAttribute("edited", "edited");
+                getServletContext().getRequestDispatcher("/WEB-INF/viewAppointment.jsp").forward(request, response);
+                return;
+            }            
         } catch (Exception ex) {
             Logger.getLogger(EditAppointmentServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
