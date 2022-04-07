a9bf868f5c50587ad00896ccc3a62643ecad2321
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-04-06 19:10:30 -0600

    Update ForgotServlet.java from Jonghan

--- src/java/servlets/ForgotServlet.java
+++ src/java/servlets/ForgotServlet.java
@@ -20,7 +20,7 @@ public class ForgotServlet extends HttpServlet {
         Account forgotAccount = (Account) session.getAttribute("forgotAccount");
         String user_name = (String) session.getAttribute("user_name");
         String query = null;
-        
+
         if (user_name != null && !user_name.equals("")) {
             response.sendRedirect("welcome");
             return;
@@ -49,29 +49,34 @@ public class ForgotServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {        
+            throws ServletException, IOException {
         HttpSession session = request.getSession();
-        Account forgotAccount = (Account) session.getAttribute("forgotAccount");        
+        Account forgotAccount = (Account) session.getAttribute("forgotAccount");
         AccountService accountService = new AccountService();
         String url = request.getRequestURL().toString();
         String path = getServletContext().getRealPath("/WEB-INF");
         String action = request.getParameter("action");
-        
+
         if (action.equals("findPwd")) {
             String email = request.getParameter("resetEmail");
-            
-            if (forgotAccount == null && email != null && !email.equals("")) {
-                Account account2 = accountService.resetPassword(email, path, url);
-                session.setAttribute("forgotAccount", account2);
-                request.setAttribute("resetMessage", "We sent an email to your email address.");
-                
-                getServletContext().getRequestDispatcher("/WEB-INF/forgot.jsp").forward(request, response);
-                return;
-            } else {
+            if (email == null || email.equals("")) {
                 request.setAttribute("resetMessage", "You should fill in the blank.");
-                
+
                 getServletContext().getRequestDispatcher("/WEB-INF/forgot.jsp").forward(request, response);
                 return;
+            } else if (forgotAccount == null && email != null && !email.equals("")) {
+                Account account2 = accountService.resetPassword(email, path, url);
+                if (account2 != null) {
+                    session.setAttribute("forgotAccount", account2);
+                    request.setAttribute("resetMessage", "We sent an email to your email address.");
+                    getServletContext().getRequestDispatcher("/WEB-INF/forgot.jsp").forward(request, response);
+                    return;
+                } else {
+                    request.setAttribute("resetMessage", "Check account information again.");
+                    getServletContext().getRequestDispatcher("/WEB-INF/forgot.jsp").forward(request, response);
+                    return;
+                }
+
             }
         } else if (action.equals("newPassword")) {
             String newPassword = request.getParameter("resetPassword");
@@ -80,21 +85,21 @@ public class ForgotServlet extends HttpServlet {
             if (newPassword != null && !newPassword.equals("") && resetConfirmPassword != null && !resetConfirmPassword.equals("")
                     && newPassword.equals(resetConfirmPassword)) {
                 accountService.changePassword(forgotAccount.getReset_password_uuid(), newPassword);
-                
+
 //                session.removeAttribute("account");
                 request.setAttribute("message", "You have successfully changed your password.");
-                
+
                 getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
                 return;
             } else if (newPassword != null && !newPassword.equals("") && resetConfirmPassword != null && !resetConfirmPassword.equals("")
                     && !newPassword.equals(resetConfirmPassword)) {
                 request.setAttribute("newPswMessage", "Password confirmation does not match.");
-                
+
                 getServletContext().getRequestDispatcher("/WEB-INF/resetNewPassword.jsp").forward(request, response);
                 return;
             } else {
                 request.setAttribute("newPswMessage", "You should fill in the blank.");
-                
+
                 getServletContext().getRequestDispatcher("/WEB-INF/resetNewPassword.jsp").forward(request, response);
                 return;
             }
@@ -102,31 +107,32 @@ public class ForgotServlet extends HttpServlet {
             String firstName = request.getParameter("firstName");
             String lastName = request.getParameter("lastName");
             String emailFind = request.getParameter("email_accountForm");
-            
-            if (firstName != null && !firstName.equals("")
-                    && lastName != null && !lastName.equals("") 
-                    && emailFind != null && !emailFind.equals("")) {
+
+            if (firstName == null || firstName.equals("")
+                    && lastName == null || lastName.equals("")
+                    && emailFind == null || emailFind.equals("")) {
+                request.setAttribute("resetPwd", "You should fill in the blank.");
+
+                getServletContext().getRequestDispatcher("/WEB-INF/forgot.jsp").forward(request, response);
+                return;
+            } else {
                 try {
                     Account checkAccount = accountService.sendAccount(emailFind, firstName, lastName, path);
-                    
-                    if (checkAccount == null) {
-                        request.setAttribute("resetPwd", "Check account information again.");
-                        
+
+                    if (checkAccount != null) {
+                        request.setAttribute("resetPwd", "We sent an email to your email address.");
                         getServletContext().getRequestDispatcher("/WEB-INF/forgot.jsp").forward(request, response);
                         return;
+
                     } else {
-                        request.setAttribute("resetPwd", "We sent an email to your email address.");
+                        request.setAttribute("resetPwd", "Check account information again.");
+
                         getServletContext().getRequestDispatcher("/WEB-INF/forgot.jsp").forward(request, response);
                         return;
                     }
                 } catch (Exception ex) {
                     Logger.getLogger(ForgotServlet.class.getName()).log(Level.SEVERE, null, ex);
                 }
-            } else {
-                request.setAttribute("resetPwd", "You should fill in the blank.");
-                
-                getServletContext().getRequestDispatcher("/WEB-INF/forgot.jsp").forward(request, response);
-                return;
             }
         }
     }
